11cf92815b4369356e40cb8d3803eb1ec8062753
RogueArt <57082175+RogueArt@users.noreply.github.com>
2021-09-09 06:18:53 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (javascript)

--- problems/longest_substring_without_repeating_characters/solution.js
+++ problems/longest_substring_without_repeating_characters/solution.js
@@ -2,61 +2,43 @@
  * @param {string} s
  * @return {number}
  */
-const lengthOfLongestSubstring = str => {
-    const strLen = str.length
+const lengthOfLongestSubstring = (s) => {
+    const len = s.length
+    if (len === 0 || len === 1) return len
+    
+    let [left, right, maxLen] = [0, 0, 1]    
+    let seenChars = {}
+    
+    // Used for O(1) tracking length
+    seenChars.length = 0
+    
+    // Make sure pointer doesn't go outside of array
+    while (right < s.length) {
+        const [leftChar, rightChar] = [s[left], s[right]]
 
-    let maxLen = 0
-    for (let offset = 0; offset < strLen; offset++) {
-        const hashMap = {}
-        let count = 0
-        
-        for (let x = offset; x < strLen; x++) {
-            const ch = str[x]
-
-            // If we haven't seen this char before, add it to hashMap
-            if (!hashMap[ch]) {
-                hashMap[ch] = true
-                count += 1
-            }
-                        
-            // If char repeats, break
-            else break
+        // Seen chars hasn't seen the right char, so add it
+        if (!seenChars[rightChar]) {
+            // Add right char to hashmap, update size
+            seenChars[rightChar] = true
+            seenChars.length += 1
+            
+            // Move right pointer more to right           
+            right += 1
+            
+            // Set max to whatever hashmap size is
+            maxLen = Math.max(seenChars.length, maxLen)
         }
-        maxLen = Math.max(count, maxLen)
+        
+        // Already saw the character
+        else {
+            // Set character in object as unseen, reduce length
+            seenChars[leftChar] = false
+            seenChars.length -= 1
+            
+            // Move left pointer more to right
+            left += 1
+        }       
     }
     
     return maxLen
 };
-
-    /*
-    const strLen = str.length
-    
-    // Go through all possible substrings of a string    
-    for (let len = strLen; len > 0; len--) {
-        for (let y = 0; y < strLen - len + 1; y++) {
-            const substr = str.substring(y, y + len)
-            
-            // If no repeated chars, then this is the longest substring
-            if (!hasRepeatedChars(substr)) return len
-        }
-    }
-    
-    return 0
-    */
-
-const hasRepeatedChars = (str) => {
-    const hashMap = {}
-    
-    // Go through each char
-    for (const char of str) {
-        
-        // If char not seen before, put it into hashmap
-        if (!hashMap[char]) { hashMap[char] = true }
-        
-        // If char repeats, return true
-        else { return true }
-    }
-    
-    // Has only unique chars
-    return false
-}
