cd17bc5852743e7943567260429e1096843875ef
RogueArt <57082175+RogueArt@users.noreply.github.com>
2021-10-17 18:53:45 +0000

    Sync LeetCode submission - Valid Palindrome (javascript)

--- problems/valid_palindrome/solution.js
+++ problems/valid_palindrome/solution.js
@@ -5,23 +5,37 @@
 const isPalindrome = (s) => {
     let [left, right] = [0, s.length - 1]
     
-    while (left < right) {
-        while (left < right && !isAlphanum(s[left])) { left++ }
-        while (left < right && !isAlphanum(s[right])) { right-- }
-
-        const leftChar = s[left].toLowerCase()
-        const rightChar = s[right].toLowerCase()
+    // Go until left pointer is right of right
+    while (left <= right) {
+        const [leftCh, rightCh] = [s[left].toLowerCase(), s[right].toLowerCase()]
         
-        if (left < right && leftChar !== rightChar) return false
-        left++, right--
+        // If it's not alphanumeric, skip one left
+        if (!isAlnum(leftCh)) { 
+            left += 1
+            continue
+        }
+        
+        // If it's not alphanumeric, skip one right
+        if (!isAlnum(rightCh)) {
+            right -= 1
+            continue
+        }
+        
+        // Check if characters are same on both sides
+        if (leftCh !== rightCh) return false
+        
+        left += 1
+        right -= 1
     }
+    
+    // Characters are same on both sides, so palindrome
     return true
 };
 
-function isAlphanum(ch) {
-    const isNum = '0' <= ch && ch <= '9'
-    const isLower = 'a' <= ch && ch <= 'z'
-    const isUpper = 'A' <= ch && ch <= 'Z'
+// Check if a character is alphanumeric
+function isAlnum(ch) {
+    if ('0' <= ch && ch <= '9') return true
+    if ('a' <= ch && ch <= 'z') return true
     
-    return isNum || isLower || isUpper
+    return false
 }
