6ca3ff3ded8cb2447ef11518782f8115653134ab
RogueArt <57082175+RogueArt@users.noreply.github.com>
2021-10-17 19:42:44 +0000

    Sync LeetCode submission - Binary Search (javascript)

--- problems/binary_search/solution.js
+++ problems/binary_search/solution.js
@@ -4,29 +4,20 @@
  * @return {number}
  */
 const search = (nums, target) => {
-    // Use a two pointer approach
-    // Two pointers on the leftmost and rightmost edge of array
     let [left, right] = [0, nums.length - 1]
     
+    // Go until left pointer is right of right pointer
     while (left <= right) {
-        // Get the midpoint and the actual value associated with it
-        const mid = left + Math.floor((right - left) / 2)
+        // Get the mid point and middle val
+        const mid = left + Math.floor((right - left)/2)
         const midVal = nums[mid]
         
-        // Three cases:
-        // Case 1: If midval is same as what we're looking for
-        // Return the value since we're done searching
+        // Reduce search space by half each time
         if (midVal === target) return mid
-        
-        // Case 2: If midval is less than what we're looking for
-        // Reduce the search space to the right half of the array
-        else if (midVal < target) left = mid + 1
-        
-        // Case 3: If midval is more than what we're looking for
-        // Reduce the search space to the left half of the array
-        else right = mid - 1
+        if (midVal < target) left = mid + 1
+        if (midVal > target) right = mid - 1
     }
     
-    // Could not find the value in the array, return sentinel value
+    // Value does not exist
     return -1
 };
