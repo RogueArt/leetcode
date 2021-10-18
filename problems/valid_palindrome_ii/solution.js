9337038291de668dcaefa3b93b0eaeb15e783194
RogueArt <57082175+RogueArt@users.noreply.github.com>
2021-10-18 02:20:50 +0000

    Sync LeetCode submission - Valid Palindrome II (javascript)

--- problems/valid_palindrome_ii/solution.js
+++ problems/valid_palindrome_ii/solution.js
@@ -2,39 +2,28 @@
  * @param {string} s
  * @return {boolean}
  */
-const validPalindrome = (s) => {
+const validPalindrome = (s) => { 
+    // Check if the string is a palindrome after removing one char from it
     let [left, right] = [0, s.length - 1]
     
-    // Go through each char while left and right pointers don't cross
-    while (left < right) {
-        
-        // Check if left and right chars are same
-        if (s.charAt(left) !== s.charAt(right)) {
-            
-            // If they aren't, check if string is palindrome with char removed
-            const withoutLeftChar = isRestPalindrome(s, left + 1, right)
-            const withoutRightChar = isRestPalindrome(s, left, right - 1)
-            
-            return withoutLeftChar || withoutRightChar
-        }
-        
-        // Increment pointers towards middle
+    while (left <= right) {
+        // If the current char doesn't match, check if rest are palindrome
+        if (s[left] !== s[right]) return checkRestPalindrome(s, left + 1, right) || checkRestPalindrome(s, left, right - 1)
+    
         left += 1
         right -= 1
     }
     
-    // Matches with at least one char not present
     return true
-};
+}
 
-// Check if rest of string is palindrome
-const isRestPalindrome = (s, left, right) => {
-  while (left < right) {
-      if (s.charAt(left) !== s.charAt(right)) return false
-      
-      left += 1
-      right -= 1
-  }
+function checkRestPalindrome(s, left, right) {
+    while (left <= right) {
+        if (s[left] !== s[right]) return false
+        
+        left += 1
+        right -= 1
+    }
     
-  return true
+    return true
 }
