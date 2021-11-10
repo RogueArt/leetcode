fe2ed2788b776f3df296f8f282e9e16c5aca43f2
RogueArt <57082175+RogueArt@users.noreply.github.com>
2021-11-10 09:38:56 +0000

    Sync LeetCode submission - Toeplitz Matrix (javascript)

--- problems/toeplitz_matrix/solution.js
+++ problems/toeplitz_matrix/solution.js
@@ -2,39 +2,29 @@
  * @param {number[][]} matrix
  * @return {boolean}
  */
-const isToeplitzMatrix = (matrix) => {
-    // Go through columns
-    for (let x = 0; x < matrix.length; x += 1) {
-        if (!isDiagonalSame(matrix, x, 0)) return false
+function diagonalIsEqual(matrix, startX, startY) {
+    const num = matrix[startX][startY]
+    while (startX < matrix.length && startY < matrix[0].length) {
+        if (num !== matrix[startX][startY]) return false
+        startX += 1
+        startY += 1
     }
     
-    // Go through rows
-    for (let y = 1; y < matrix[0].length; y += 1) {
-        if (!isDiagonalSame(matrix, 0, y)) return false
+    return true
+}
+
+const isToeplitzMatrix = (matrix) => {
+    // Traverse from bottom left to top left
+    for (let y = matrix[0].length - 1; y >= 0; y -= 1) {
+        // Check Toeplitz
+        if (!diagonalIsEqual(matrix, 0, y)) return false
     }
- 
+    
+    // Traverse from top left to top right
+    for (let x = 0; x < matrix.length; x += 1) {
+        // Check Toeplitz
+        if (!diagonalIsEqual(matrix, x, 0)) return false
+    }
+    
     return true
 };
-
-// Check if diagonal of matrix is same
-function isDiagonalSame(matrix, x, y) {
-    const [m, n] = [matrix.length, matrix[0].length]
-    const firstVal = matrix[x][y]
-    
-    while (!isOutOfBounds(x, y, m, n)) {
-        // Check if values match 
-        if (matrix[x][y] !== firstVal) return false
-        
-        x += 1
-        y += 1
-    }
-    return true
-}
-
-// Checks if the coordinate is out of bounds
-function isOutOfBounds(x, y, m, n) {
-    const outOfX = x < 0 || x >= m 
-    const outOfY = y < 0 || y >= n
-    
-    return outOfX || outOfY
-}
