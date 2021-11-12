92c597e0460ef0b682365fdb0c618515a1613126
RogueArt <57082175+RogueArt@users.noreply.github.com>
2021-11-12 06:20:20 +0000

    Sync LeetCode submission - Number of Islands (javascript)

--- problems/number_of_islands/solution.js
+++ problems/number_of_islands/solution.js
@@ -2,33 +2,40 @@
  * @param {character[][]} grid
  * @return {number}
  */
-const numIslands = (grid) => {
-    const [m, n] = [grid.length, grid[0].length]
-    
+function numIslands(grid) {
     let numIslands = 0
-    for (let x = 0; x < m; x += 1) {
-        for (let y = 0; y < n; y += 1) {
+    for (let x = 0; x < grid.length; x += 1) {
+        for (let y = 0; y < grid[0].length; y += 1) {
             if (grid[x][y] === '1') {
-                numIslands += 1
                 sinkIsland(grid, x, y)
+                numIslands += 1
             }
         }
     }
-    
     return numIslands
 };
 
-function isInvalidIndex(grid, x, y) {    
-    return (x < 0 || x >= grid.length) || (y < 0 || y >= grid[0].length) || grid[x][y] === '0'
-}
-
+// Go through neighbors, sink islands as we see them
+const neighbors = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 function sinkIsland(grid, x, y) {
-    if (isInvalidIndex(grid, x, y)) return
+    if (grid[x][y] !== '1') return
     
-    // Sink the island by setting island to 0's
-    grid[x][y] = '0'
-    sinkIsland(grid, x + 1, y)
-    sinkIsland(grid, x - 1, y)
-    sinkIsland(grid, x, y + 1)
-    sinkIsland(grid, x, y - 1)
+    // Mark the current as visited
+    grid[x][y] = "0"
+    
+    // Proceed to explore the rest of the island
+    for (const neighbor of neighbors) {
+        const newX = neighbor[0] + x
+        const newY = neighbor[1] + y
+        
+        if (!isOOB(grid, newX, newY)) sinkIsland(grid, newX, newY)
+    }
+}
+    
+// Check if the coordinate is out of bounds of grid
+function isOOB(grid, x, y) {
+    if (x > grid.length - 1 || x < 0) return true
+    if (y > grid[0].length - 1 || y < 0) return true
+    
+    return false
 }
