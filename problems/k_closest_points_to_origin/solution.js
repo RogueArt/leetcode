2e10b33c0d4728dcdae8ddafe6f69682e4da489a
RogueArt <57082175+RogueArt@users.noreply.github.com>
2021-12-26 03:52:37 +0000

    Sync LeetCode submission - K Closest Points to Origin (javascript)

--- problems/k_closest_points_to_origin/solution.js
+++ problems/k_closest_points_to_origin/solution.js
@@ -1,23 +1,43 @@
+
 /**
  * @param {number[][]} points
  * @param {number} k
  * @return {number[][]}
  */
-const kClosest = (points, k) => {
-    points.sort((a, b) => {
-        const distA = getDistanceToOrigin(a)
-        const distB = getDistanceToOrigin(b)
-        
-        return distA - distB
+function kClosest(points, k) {    
+    // Initialize a priority queue maximizing distance
+    const pointsQueue = new MinPriorityQueue({
+        compare: (p1, p2) => {
+            const distA = getSquareDistanceBetween(p1)
+            const distB = getSquareDistanceBetween(p2)
+            
+            return distB - distA
+        }
     })
     
-    const res = []
-    for (let x = 0; x < k; x += 1) {
-        res.push(points[x])
+    // Add points to heap
+    for (const point of points) {        
+        // If size is smaller than k, just add it
+        if (pointsQueue.size() < k) pointsQueue.enqueue(point)
+    
+        // Otherwise, peek and remove farthest if closer is found
+        else if (getSquareDistanceBetween(point) < getSquareDistanceBetween(pointsQueue.front())) {
+            pointsQueue.dequeue()
+            pointsQueue.enqueue(point)
+        }
     }
+    
+    // Convert heap into array
+    const res = []
+    for (let x = 0; x < k; x += 1)
+        res.push(pointsQueue.dequeue())
+    
     return res
 };
 
-function getDistanceToOrigin(point) {
-    return Math.sqrt(point[0] ** 2 + point[1] ** 2)
+function getSquareDistanceBetween(pointA, pointB = [0, 0]) {
+    const distX = (pointA[0] ** 2) - (pointB[0] ** 2)
+    const distY = (pointA[1] ** 2) - (pointB[1] ** 2)
+
+    return distX + distY
 }
