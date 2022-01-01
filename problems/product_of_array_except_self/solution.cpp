cd500e558836c67a93e27a864dd8d4f7869030d8
RogueArt <57082175+RogueArt@users.noreply.github.com>
2022-01-01 02:53:20 +0000

    Sync LeetCode submission - Product of Array Except Self (cpp)

--- problems/product_of_array_except_self/solution.cpp
+++ problems/product_of_array_except_self/solution.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> pfx, sfx;
-        pfx.push_back(nums[0]);
-        sfx.push_back(nums[nums.size()-1]);
+        int pfx[nums.size()], sfx[nums.size()];
+        pfx[0] = nums[0];
+        sfx[nums.size()-1] = nums[nums.size()-1];
         for (int i = 1; i < nums.size(); ++i) {
-           pfx.push_back(pfx[i-1]*nums[i]); 
-           sfx.push_back(sfx[i-1]*nums[nums.size()-1-i]);
+            pfx[i] = nums[i] * pfx[i-1];
+            sfx[nums.size()-1-i] = nums[nums.size()-1-i] * sfx[nums.size()-i];
         }
-        vector<int> ans;
-        for (int i = 0; i < nums.size(); ++i) {
-           int x = 1; 
-           if (i != 0) x = pfx[i-1];
-           if (i != nums.size()-1) x *= sfx[nums.size()-2-i];
-           ans.push_back(x);
+        vector<int> ans; ans.resize(nums.size());
+        ans[0] = sfx[1]; ans[nums.size()-1] = pfx[nums.size()-2];
+        for (int i = 1; i < nums.size()-1; ++i) {
+            ans[i] = pfx[i-1] * sfx[i+1];
         }
         return ans;
     }
