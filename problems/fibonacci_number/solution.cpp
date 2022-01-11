3e50d25be788c1e4cc41462cd3a8700b6b47ccd2
RogueArt <57082175+RogueArt@users.noreply.github.com>
2022-01-11 20:35:59 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-      if (n == 0) return 0;
-      if (n == 1) return 1;
-        int dp[n+1]; dp[0] = 0; dp[1] = 1;
-        for (int i = 2; i < n+1; ++i) dp[i] = dp[i-1] + dp[i-2];
-      return dp[n];
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int dp[n+1]; memset(dp, 0, sizeof(dp));
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
